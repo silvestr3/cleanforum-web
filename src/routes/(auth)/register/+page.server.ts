c0c25cf66f95934f3ea026466a351abc7ba6bc7b
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-15 20:36:23 -0300

    Add server action to register new user

---
+++ src/routes/(auth)/register/+page.server.ts
@@ -0,0 +1,34 @@
+import { env } from '$lib/env/index.js';
+import { redirect, type RequestEvent } from '@sveltejs/kit';
+
+export const actions = {
+	default: async ({ request }: RequestEvent) => {
+		const data = await request.formData();
+		const name = data.get('name');
+		const email = data.get('email');
+		const password = data.get('password');
+
+		const result = await fetch(`${env.API_URL}/accounts`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name,
+				email,
+				password
+			})
+		});
+
+		if (result.status === 201) {
+			redirect(302, '/login?created');
+		} else {
+			const { message } = await result.json();
+
+			return {
+				status: result.status,
+				message
+			};
+		}
+	}
+};
