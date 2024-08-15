f554a4ff039c12b5996d957ad1ba9f15e16a049c
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-15 18:24:22 -0300

    Add registration page

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -3,10 +3,12 @@
 		<h1 class="title">Clean Forum</h1>
 
 		<div class="input-wrapper">
+			<input name="name" type="text" placeholder="Name" />
 			<input name="email" type="text" placeholder="Email" />
 			<input name="password" type="password" placeholder="Password" />
+			<input name="confirm-password" type="password" placeholder="Confirm Password" />
 		</div>
-		<button type="submit">Login</button>
-		<a href="/register">Register</a>
+		<button type="submit">Create account</button>
+		<a href="/login">Already have an account? Sign-in</a>
 	</form>
 </div>
