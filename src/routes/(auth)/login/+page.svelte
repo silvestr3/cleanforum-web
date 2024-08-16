997f9ee40899ec6e820f7b0ef3a707ced2e011e8
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-16 15:37:36 -0300

    Add visual confirmation for user registration

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,3 +1,15 @@
+<script>
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
+	import { toast } from 'svelte-sonner';
+
+	let isCreatedNewAccount = $page.url.searchParams.get('created');
+
+	$: if (isCreatedNewAccount !== null) {
+		toast.success('Account created successfully!');
+	}
+</script>
+
 <div class="container">
 	<form method="POST" class="box">
 		<h1 class="title">Clean Forum</h1>
