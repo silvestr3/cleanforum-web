05aed980d7cfc983437c67950e77a9177ed1bc26
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-16 18:04:54 -0300

    Add login functionality

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,11 +1,38 @@
-import type { RequestEvent } from '@sveltejs/kit';
+import { env } from '$lib/env/index.js';
+import { fail, redirect, type Actions, type RequestEvent } from '@sveltejs/kit';
 
-export const actions = {
-	default: async ({ request }: RequestEvent) => {
+export const actions: Actions = {
+	default: async ({ request, cookies }: RequestEvent) => {
 		const data = await request.formData();
 		const email = data.get('email');
 		const password = data.get('password');
 
-		console.log(email, password);
+		const result = await fetch(`${env.API_URL}/sessions`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email,
+				password
+			})
+		});
+
+		const responseData = await result.json();
+
+		if (result.status === 201) {
+			const { access_token } = responseData;
+			cookies.set('access_token', access_token, {
+				path: '/',
+				httpOnly: true,
+				sameSite: true,
+				secure: true
+			});
+
+			return redirect(302, '/home');
+		} else {
+			const { message } = responseData;
+			return fail(result.status, { message });
+		}
 	}
 };
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,9 +1,13 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import { LoaderCircleIcon } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
 
 	let isCreatedNewAccount = $page.url.searchParams.get('created');
+
+	let isLoading = false;
 
 	$: if (isCreatedNewAccount !== null) {
 		toast.success('Account created successfully!');
@@ -11,14 +15,42 @@
 </script>
 
 <div class="container">
-	<form method="POST" class="box">
+	<form
+		method="POST"
+		class="box"
+		use:enhance={() => {
+			isLoading = true;
+			return ({ result }) => {
+				const { type } = result;
+
+				if (type === 'failure') {
+					const { data } = result;
+					const message = data ? String(data.message) : 'Unexpected error';
+
+					toast.error(message);
+				}
+
+				if (type === 'redirect') {
+					goto(result.location);
+				}
+
+				isLoading = false;
+			};
+		}}
+	>
 		<h1 class="title">Clean Forum</h1>
 
 		<div class="input-wrapper">
 			<input name="email" type="text" placeholder="Email" />
 			<input name="password" type="password" placeholder="Password" />
 		</div>
-		<button type="submit">Login</button>
+		<button type="submit">
+			{#if !isLoading}
+				Login
+			{:else}
+				<LoaderCircleIcon />
+			{/if}
+		</button>
 		<a href="/register">Register</a>
 	</form>
 </div>
